9000664385347a090ee037aa58f92cb26181f7d2
Starry <roharv10@gmail.com>
2025-03-24 15:42:20 -0500

    test of commits

--- src/main/java/net/starry/birdofhermes/BirdOfHermes.java
+++ src/main/java/net/starry/birdofhermes/BirdOfHermes.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//commit test
 public class BirdOfHermes implements ModInitializer {
 	public static final String MOD_ID = "birdofhermes";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
