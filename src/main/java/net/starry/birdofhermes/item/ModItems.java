ad1970e9c286ed8bbaa999e106e20b74614ceb96
Starry <roharv10@gmail.com>
2025-03-24 20:00:44 -0500

    Holy blood added

--- src/main/java/net/starry/birdofhermes/item/ModItems.java
+++ src/main/java/net/starry/birdofhermes/item/ModItems.java
@@ -1,23 +1,25 @@
 package net.starry.birdofhermes.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.starry.birdofhermes.BirdOfHermes;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registry;
 
 public class ModItems {
-    public static final Item HOLY_BLOOD = registerItem("holy_blood", new Item(new Item.Settings()));
+    public static final Item HOLY_BLOOD = registerItem("holy_blood", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BirdOfHermes.MOD_ID,"holy_blood")))));
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(BirdOfHermes.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
-        BirdOfHermes.LOGGER.info("Registering Mod Items for " + BirdOfHermes.MOD_ID);
+        BirdOfHermes.LOGGER.info("registering Mod Items for" + BirdOfHermes.MOD_ID);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(HOLY_BLOOD);
